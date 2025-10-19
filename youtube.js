7e3cdff3bfd5755de7d100619149a1a182e5e3b0
Aakib <syedaakibhossain@gmail.com>
2025-10-19 10:40:14 -0400

    Add files via upload

---
+++ youtube.js
@@ -0,0 +1,36 @@
+function linkone(){
+    window.location.href = "https://www.youtube.com/watch?v=n2RNcPRtAiY";
+}
+function linktwo(){
+    window.location.href = "https://www.youtube.com/watch?v=mP0RAo9SKZk";
+}
+function linkthree(){
+    window.location.href = "https://www.youtube.com/watch?v=FgjPQQeTh1w";
+}
+function linkfour(){
+    window.location.href = "https://www.youtube.com/watch?v=094y1Z2wpJg";
+}
+function linkfive(){
+    window.location.href = "https://www.youtube.com/watch?v=86CQq3pKSUw";
+}
+function linksix(){
+    window.location.href = "https://www.youtube.com/watch?v=yXWw0_UfSFg";
+}
+function linkseven(){
+    window.location.href = "https://www.youtube.com/watch?v=fNVa1qMbF9Y";
+}
+function linkeight(){
+    window.location.href = "https://www.youtube.com/watch?v=lFm4EM1juls";
+}
+function linknine(){
+    window.location.href = "https://www.youtube.com/watch?v=ixmxOlcrlUc";
+}
+function linkten(){
+    window.location.href = "https://www.youtube.com/watch?v=R2vXbFp5C9o";
+}
+function linkele(){
+    window.location.href = "https://www.youtube.com/watch?v=0nZuYyXET3s";
+}
+function linktwe(){
+    window.location.href = "https://www.youtube.com/watch?v=9iMGFqMmUFs";
+}
